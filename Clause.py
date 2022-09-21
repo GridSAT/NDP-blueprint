a8f6dc9a39d97e4eb4689bc1fa93be9c994e0658
Karim <78545604+alephc1@users.noreply.github.com>
2022-09-21 20:35:43 +0200

    Update Clause.py

--- Clause.py
+++ Clause.py
@@ -1,3 +1,26 @@
+<!--
+#	README.md
+#
+#	Non-Deterministic Processor (NDP)
+#	Copyright (c) 2022 GridSAT Stiftung
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU Affero General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU Affero General Public License for more details.
+#
+#	You should have received a copy of the GNU Affero General Public License
+#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#	GridSAT Stiftung - Georgstr. 11 - 30159 Hannover - Germany - info@gridsat.io
+#
+-->
+
 class Clause:
     
     def __init__(self, inp):
