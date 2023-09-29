66e210d3a2cd5863df8c2bf4dbbeb78585dcffac
Karim <78545604+alephc1@users.noreply.github.com>
2023-09-29 14:49:55 +0200

    Update Clause.py
    removed html tags

--- Clause.py
+++ Clause.py
@@ -1,4 +1,3 @@
-<!--
 #	Clause.py
 #
 #	Non-Deterministic Processor (NDP) blueprint
@@ -8,7 +7,7 @@
 #	it under the terms of the GNU Affero General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	(at your option) any later version.
-
+#
 #	This program is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,17 +15,10 @@
 #
 #	You should have received a copy of the GNU Affero General Public License
 #	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#	GridSAT Stiftung - Georgstr. 11 - 30159 Hannover - Germany - ipfs: gridsat.eth/ - info@gridsat.io
+#
 
-#	GridSAT Stiftung - Georgstr. 11 - 30159 Hannover - Germany - info@gridsat.io
-#
--->
-
-# Clause.py
-#
-# Copyright © 2016 - 2021 EasyXPS, Inc. <info@easyxps.com>
-# All rights reserved.
-# Simply Efficient. is a trademark of EasyXPS, Inc.
-#
 
 class Clause:
 
